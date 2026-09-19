77e316c685daf9cbc6e9cadf2a2a0c9445932d1a
kakaomame <nattyannkunndesu@icloud.com>
2026-09-19 10:47:39 +0900

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,2 +1,13 @@
 curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json -o v-m.json
-jq. v-m.json > version.json
+jq . v-m.json > version.json
+echo "[" > id.json
+echo "[" > type.json
+echo "[" > url.json
+jq .versions[].id version.json >> id.json
+jq .versions[].url version.json >> url.json
+jq .versions[].type version.json >> type.json
+
+
+echo "]" >> id.json
+echo "]" >> type.json
+echo "]" >> url.json
